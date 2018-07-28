dc67e961068ec2895643a4ac4cf5d4066aefa92e
chenhongwen16 <309861722@qq.com>
2018-07-28 17:53:57 +0800

    Add the answer

--- python基础知识/面试题.py
+++ python基础知识/面试题.py
@@ -29,7 +29,7 @@
 9.
 10.
 11.999
-    
+
     def f(n):
         n += 1
         f(n)
@@ -40,5 +40,116 @@
 14.机器码(machine code)，学名机器语言指令，有时也被称为原生码（Native Code），是电脑的CPU可直接解读的数据。
     字节码（Bytecode）是一种包含执行程序、由一序列 op 代码/数据对 组成的二进制文件。字节码是一种中间码，它比机器码更抽象，需要直译器转译后才
     能成为机器码的中间代码。
-15.
+15.c= a if a>1 else b
+16.Python3中print为一个函数，必须用括号括起来；Python2中print为class
+   Python3中input得到的为str；Python2的input的到的为int型，Python2的raw_input得到的为str类型
+   Python3中/表示真除，%表示取余，//结果取整；Python2中带上小数点/表示真除，%表示取余，//结果取整
+17.a,b = b,a
+18
+19.xrange 用法与 range 完全相同，所不同的是生成的不是一个list对象，而是一个生成器。
+20.xreadlines返回一个生成器，来循环操作文件的每一行。
+21.‘’ False 0 [] () {}
+22.str: split() strip() isalpha() join() count() upper() replace()
+   list:append() index() pop() reverse() remove() count()
+   dict:get() items() update() clear()
+   https://www.cnblogs.com/nianlei/p/5642315.html
+23.lambda x : x+x  配合map reduce filter使用
+24.语义完整性
+25.*args：可以理解为只有一列的表格，长度不固定。
+   def sum(*args):
+      count = 0
+      for i in args:
+          count += i
+      print(count)
+   sum(1,2,3)
+   **kwargs：可以理解为字典，长度也不固定。
+   def fun(**kwargs):
+      for key in kwargs:
+          print("person's info: %s %s"%(key,kwargs[key]))
+   fun(chw='27',zch='23')
+26.is对比的是数据地址，==比较的是value
+27.
+28.
+29.可变类型： 字典、列表
+   不可变类型： 字符串、元祖、数字
+30. {'k2': [666], 'k1': [666]}
+    False
+    {'k2': [666], 'k1': 777}
+31.[6,6,6,6]作用域   好好看下
+32.
+33.def f(x,y):
+      return x+y
+   print(reduce(f,[1,2,3,4]))
+   from functools import reduce
+   print(reduce(lambda x,y:x+y,[1,2,3,4]))
+   print(list(map(lambda n:n*2,range(10))))
+   print(list(filter(lambda x:x>2,range(10))))
+34.print('\n'.join(['\t'.join(["%2s*%2s=%2s"%(j,i,i*j) for j in range(1,i+1)]) for i in range(1,10)]))
+35.
+36.re.os.time.math.random.sys.Django.pip.pygame.pyMysql.numpy
+37.print(re.match('super','superstition').span())
+   # print(re.match('super','insuperable').span())
+   print(re.search('super','superstition').span())
+   print(re.search('super','insuperable').span())
+   (0,5)
+   报错
+   (0,5)
+   (2,7)
+38.String str="abcaxc";
+   Patter p="ab*c";
+   贪婪匹配:正则表达式一般趋向于最大长度匹配，也就是所谓的贪婪匹配。如上面使用模式p匹配字符串str，结果就是匹配到：abcaxc(ab*c)。
+   非贪婪匹配：就是匹配到结果就好，就少的匹配字符。如上面使用模式p匹配字符串str，结果就是匹配到：abc(ab*c)。
+39.[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+   [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5]
+40.
+41.
+42.split(',')
+43.
+44.
+45.print(list(map(lambda x:x*x,range(1,11))))
+   [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+46.先set在list
+47.global
+48.logging模块的作用？以及应用场景
+49.class Stack(object):
+    # 初始化栈为空列表
+    def __init__(self):
+        self.items = []
+
+    # 判断栈是否为空，返回布尔值
+    def is_empty(self):
+        return self.items == []
+
+    # 返回栈顶元素
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    # 返回栈的大小
+    def size(self):
+        return len(self.items)
+
+    # 把新的元素堆进栈里面（程序员喜欢把这个过程叫做压栈，入栈，进栈……）
+    def push(self, item):
+        self.items.append(item)
+
+    # 把栈顶元素丢出去（程序员喜欢把这个过程叫做出栈……）
+    def pop(self, item):
+        return self.items.pop()
+50.
+51.
+52.def search(list,target):
+      high = len(list) - 1
+      low = 0
+      while low <= high:
+          mid = (high + low) // 2
+          if list[mid] == target:
+              return mid
+          elif list[mid] > target:
+              high = mid - 1
+          else:
+              low = mid + 1
+      return -1
+   ret = search(list(range(1,1000)),0)
+   print(ret)
+53.
 '''
